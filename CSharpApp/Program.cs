8bd64c4bed97df1ed94a149ed0741972abdc176f
matthewhersh <mattmhersh@gmail.com>
2013-08-03 17:55:29 -0400

    Updated Test

--- CSharpApp/Program.cs
+++ CSharpApp/Program.cs
@@ -1,12 +1,55 @@
 using System;
 using CSharpLibrary;
+using System.Collections.Generic;
 
 namespace CSharpApp
 {
 	class MainClass
 	{
+
+		class MyList<T> : List<T> {
+
+			public event EventHandler OnAdd;
+
+			public void Add(T item) {
+				if (null != OnAdd) {
+					OnAdd(this, null);
+				}
+				base.Add(item);
+			}
+		}
+
+		static void l_OnAdd(object sender, EventArgs e) {
+			Console.WriteLine("Element added...");
+		}
+
 		public static void Main (string[] args)
 		{
+
+			// Events
+			MyList<int> l = new MyList<int>();
+			l.OnAdd += new EventHandler(l_OnAdd);
+			l.Add(1);
+			l.Add(2);
+
+			int k = 0;
+			while (k < 3) {
+				Console.WriteLine(k); 
+				k++;
+			}
+
+			const float pi = 3.1415927f; 
+			const int r = 25; 
+			Console.WriteLine(pi * r * r);
+
+			byte b = 10;
+			Console.WriteLine (b);
+
+			int i = 123;
+			object o = i; // Boxing 
+			int j = (int)o; // Unboxing
+
+			Console.WriteLine(j);
 			Console.WriteLine (Person.Name());
 
 			Stack s = new Stack(); 
