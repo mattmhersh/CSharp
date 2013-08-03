dea681c19d7319f0b055394d22e432a4e2753a0b
matthewhersh <mattmhersh@gmail.com>
2013-08-03 17:25:09 -0400

    Updates.

--- CSharpApp/Program.cs
+++ CSharpApp/Program.cs
@@ -8,6 +8,16 @@ namespace CSharpApp
 		public static void Main (string[] args)
 		{
 			Console.WriteLine (Person.Name());
+
+			Stack s = new Stack(); 
+			s.Push(1);
+			s.Push(10);
+			s.Push(100); 
+
+			Console.WriteLine(s.Pop()); 
+			Console.WriteLine(s.Pop()); 
+			Console.WriteLine(s.Pop());		
+
 			Console.ReadLine ();
 		}
 	}
